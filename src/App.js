fb5ec4a6006c6961ba3bbdc8e5d2d93413ec4f29
Gilyerm <gilyerm@gmail.com>
2020-01-27 18:22:24 +0200

    show vehicle locations

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'fetch-json';
+
+import { Button } from 'react-bootstrap';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Map from "./components/Map";
+
+
+class App extends React.Component{
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = { apiResponse : [] }
+  }
+
+  callAPI(){
+    fetch("http://localhost:9000/testApi")
+        .then(res => res.json())
+        // .then(res => JSON.parse(res))
+        .then(res =>  this.setState({ apiResponse : res }))
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            
+            <Map vehicles={this.state.apiResponse}/>
+            <p>
+              {/*{this.state.apiResponse}*/}
+            </p>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Map.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import L from 'leaflet';
+import '../App.css'
+
+import {Helmet} from "react-helmet";
+
+
+class Map extends React.Component {
+
+    vehicles = [];
+    map;
+    constructor(props, context) {
+        super(props, context);
+        this.vehicles = props.vehicles;
+    }
+
+    componentDidMount() {
+        // create map
+        this.map = L.map('map', {
+            center: [51.509865, -0.118092],
+            zoom: 10,
+            layers: [
+                L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                }),
+            ]
+        });
+    }
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        if (this.vehicles === nextProps.vehicles){
+            return false;
+        }else{
+            this.vehicles = nextProps.vehicles;
+            // console.log(this.vehicles);
+            for(let vehicle of this.vehicles){
+                console.log(vehicle);
+                let marker = L.marker(vehicle.location);
+                marker.on('click',()=>{console.log(vehicle.id)});
+                marker.addTo(this.map)
+            }
+            return true;
+        }
+    }
+
+    render() {
+        return (
+                <div id="map">
+                    <Helmet>
+                        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"/>
+                    </Helmet>
+                </div>
+        )
+    }
+}
+
+export default Map;
